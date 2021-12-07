e9cbb41e19dc03369468ee3e601fddf10f3db4b0
wuxinyi <wuxinyi@qiniu.com>
2021-12-07 18:50:02 +0800

    add spx snippet

--- src/gopMain.ts
+++ src/gopMain.ts
@@ -2,7 +2,7 @@
 
 import vscode = require('vscode');
 import { GoPlusCompletionItemProvider } from './gopSuggest';
-import { GOP_MODE } from './gopMode';
+import { GOP_MODE,SPX_MODE } from './gopMode';
 import { GoPlusDocumentFormattingEditProvider } from './gopFormat';
 import { GoHoverProvider } from './gopExtraInfo';
 import { GoDefinitionProvider } from './goDeclaration';
@@ -13,6 +13,11 @@ export function activate(ctx: vscode.ExtensionContext): void {
     vscode.languages.registerDocumentFormattingEditProvider(GOP_MODE, new GoPlusDocumentFormattingEditProvider())
     vscode.languages.registerHoverProvider(GOP_MODE, new GoHoverProvider());
     vscode.languages.registerDefinitionProvider(GOP_MODE, new GoDefinitionProvider());
+
+    vscode.languages.registerCompletionItemProvider(SPX_MODE, new GoPlusCompletionItemProvider(ctx.globalState), '.', '"')
+    vscode.languages.registerDocumentFormattingEditProvider(SPX_MODE, new GoPlusDocumentFormattingEditProvider())
+    vscode.languages.registerHoverProvider(SPX_MODE, new GoHoverProvider());
+    vscode.languages.registerDefinitionProvider(SPX_MODE, new GoDefinitionProvider());
 
     vscode.commands.registerCommand('goplus.import.add', (arg) => {
         return addImport(arg);
